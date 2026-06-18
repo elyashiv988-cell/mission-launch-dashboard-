bde3e5d88b3ed7ee3d37081c7e241118922a23a9
Elyashiv <elyashiv988@gmail.com>
2026-06-18 14:46:03 +0300

    added task

---
+++ mission_control.py
@@ -0,0 +1,7 @@
+#namber 2
+agent_name= "david cohen"
+mission_code= 693
+distance_to_target= 5.3
+mission_status= False
+#namber 3
+print(f"agent name: {agent_name} ,mission_code : {mission_code} , distance to target: {distance_to_target}k.m , mission status: {mission_status}")

28c242d834d220c9d457bfd5213a8f9433c762e7
Elyashiv <elyashiv988@gmail.com>
2026-06-18 15:11:09 +0300

    added fuel usage

--- mission_control.py
+++ mission_control.py
@@ -1,7 +1,7 @@
 #namber 2
 agent_name= "david cohen"
 mission_code= 693
-distance_to_target= 5.3
+distance_to_target= 5.5
 mission_status= False
 #namber 3
 print(f"agent name: {agent_name} ,mission_code : {mission_code} , distance to target: {distance_to_target}k.m , mission status: {mission_status}")
@@ -10,3 +10,10 @@ print(type(agent_name))
 print(type(mission_code))
 print(type(distance_to_target))
 print(type(mission_status))
+#namber 5
+base_to_target_and_back=distance_to_target*2
+print(base_to_target_and_back)
+#namber 6
+fuel_usage=3
+fuel_usage=int(fuel_usage)
+print(base_to_target_and_back*fuel_usage)

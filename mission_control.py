aa4e2a185b880315bd4d78fc4d114071bd90e463
Elyashiv <elyashiv988@gmail.com>
2026-06-18 16:16:36 +0300

    added time

--- mission_control.py
+++ mission_control.py
@@ -12,8 +12,21 @@ print(type(distance_to_target))
 print(type(mission_status))
 #namber 5
 base_to_target_and_back=distance_to_target*2
-print(base_to_target_and_back)
+print(f"distans of trip: {base_to_target_and_back}")
 #namber 6
 fuel_usage=3
 fuel_usage=int(fuel_usage)
-print(base_to_target_and_back*fuel_usage)
+fuel_usage_for_mission=base_to_target_and_back*fuel_usage
+print(f"fuel usage for mission: {fuel_usage_for_mission}")
+#namer 7
+total_fuel=50
+total_fuel=int(total_fuel)
+total_fuel_after_mission=total_fuel-fuel_usage_for_mission
+print(f"total fuel after mission: {total_fuel_after_mission}")
+#namber 8
+time=input("in how many minutes does the mission start?")
+time=int(time)
+print(f"the mission starts in {time*60} secends")
+print(f"the mission starts in {time} minutes")
+print(f"the mission starts un {time/60} houres")
+

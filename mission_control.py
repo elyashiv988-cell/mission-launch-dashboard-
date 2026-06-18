d706c22cf98bc4b9d3f0bdbd1403df162378cf64
Elyashiv <elyashiv988@gmail.com>
2026-06-18 14:52:46 +0300

    added types

--- mission_control.py
+++ mission_control.py
@@ -5,3 +5,8 @@ distance_to_target= 5.3
 mission_status= False
 #namber 3
 print(f"agent name: {agent_name} ,mission_code : {mission_code} , distance to target: {distance_to_target}k.m , mission status: {mission_status}")
+#namer 4
+print(type(agent_name))
+print(type(mission_code))
+print(type(distance_to_target))
+print(type(mission_status))
